47ddb09a9599e96fa990812d8b537dae3dbd7735
Daniel Pencak <danielpencak@gmail.com>
2016-11-11 10:22:17 -0800

    Complete bonus

--- script.js
+++ script.js
@@ -11,40 +11,80 @@
 (function() {
   'use strict';
   const h1s = document.querySelectorAll('h1');
-
+  //
   const div = document.getElementById('controls');
-  for (const h1 of h1s) {
-    h1.addEventListener('mouseover', () => {
-      console.log(`Entered ${h1.textContent} button`);
-    });
-  }
-
-  for (const h1 of h1s) {
-    h1.addEventListener('mouseout', () => {
-      console.log(`Left ${h1.textContent} button`);
-    });
-  }
-
+  // for (const h1 of h1s) {
+  //   h1.addEventListener('mouseover', () => {
+  //     console.log(`Entered ${h1.textContent} button`);
+  //   });
+  // }
+  //
+  // for (const h1 of h1s) {
+  //   h1.addEventListener('mouseout', () => {
+  //     console.log(`Left ${h1.textContent} button`);
+  //   });
+  // }
+  //
+  // const redLight = document.getElementById('stopLight');
+  // const yellowLight = document.getElementById('slowLight');
+  // const greenLight = document.getElementById('goLight');
+  //
+  // div.addEventListener('click', (event) => {
+  //   const h1 = event.target.textContent;
+  //       if (h1 === 'Stop') {
+  //         redLight.setAttribute('style', 'background-color: red');
+  //         greenLight.setAttribute('style', 'background-color: #111');
+  //         yellowLight.setAttribute('style', 'background-color: #111');
+  //       }
+  //       if (h1 === 'Slow') {
+  //         yellowLight.setAttribute('style', 'background-color: yellow');
+  //         greenLight.setAttribute('style', 'background-color: #111');
+  //         redLight.setAttribute('style', 'background-color: #111');
+  //       }
+  //       if (h1 === 'Go') {
+  //         goLight.setAttribute('style', 'background-color: green');
+  //         yellowLight.setAttribute('style', 'background-color: #111');
+  //         redLight.setAttribute('style', 'background-color: #111');
+  //       }
+  // });
   const redLight = document.getElementById('stopLight');
   const yellowLight = document.getElementById('slowLight');
   const greenLight = document.getElementById('goLight');
 
   div.addEventListener('click', (event) => {
     const h1 = event.target.textContent;
-        if (h1 === 'Stop') {
-          redLight.setAttribute('style', 'background-color: red');
-          greenLight.setAttribute('style', 'background-color: #111');
-          yellowLight.setAttribute('style', 'background-color: #111');
-        }
-        if (h1 === 'Slow') {
-          yellowLight.setAttribute('style', 'background-color: yellow');
-          greenLight.setAttribute('style', 'background-color: #111');
-          redLight.setAttribute('style', 'background-color: #111');
-        }
         if (h1 === 'Go') {
-          goLight.setAttribute('style', 'background-color: green');
+          greenLight.setAttribute('style', 'background-color: green');
           yellowLight.setAttribute('style', 'background-color: #111');
           redLight.setAttribute('style', 'background-color: #111');
+
+        }
+  });
+
+  div.addEventListener('click', (event) => {
+    const h1 = event.target.textContent;
+    if (h1 === 'Stop') {
+      greenLight.setAttribute('style', 'background-color: #111');
+      // setTimeout(function() {
+      // console.log(event.target);
+      // yellowLight.setAttribute('style', 'background-color: yellow');
+      // }, 3000);
+      // yellowLight.setAttribute('style', 'background-color: #111');
+      // redLight.setAttribute('style', 'background-color: red');
+      yellowLight.setAttribute('style', 'background-color: yellow');
+      // }, 1000);
+      setTimeout(function() {
+        yellowLight.setAttribute('style', 'background-color: #111');
+        redLight.setAttribute('style', 'background-color: red');
+      }, 2500);
+
+      // setTimeout(function() {
+      //   redLight.setAttribute('style', 'background-color: red');
+      // }, 4000);
+
+      // yellowLight.setAttribute('style', 'background-color: #111');
+
+
         }
   });
 })();
